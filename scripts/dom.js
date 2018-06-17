0f0c06d38d28695f3d57fab0c14250b5190a8731
foxed <alexdysonfox@gmail.com>
2018-06-17 15:33:43 -0400

    add about icon

--- scripts/dom.js
+++ scripts/dom.js
@@ -15,6 +15,7 @@ $(function () {
         handle: "#pnav_photo"
     });
     $(".icon").draggable();
+    $(".about_icon").draggable();
     $('#canvas').click(function () {
         changeColors();
         clear();
@@ -22,11 +23,6 @@ $(function () {
     $('.close').click(function () {
         close($(this).closest(".win"));
     });
-    function openSoundcloud() {
-        window.location.href = "http://www.soundcloud.com/lumiset";
-        handleClick('soundCloud')
-    }
-
     function openContact() {
         $("#win_contact").show();
         drawInPosition();
@@ -48,40 +44,19 @@ $(function () {
         return ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
     }
 
-
-    function openTrack(elem) {
-        var trackNbr = elem.attr('id');
-        changeColors();
-        changeNote(trackNbr);
-        stopCurrentTrackAndStartTrack(trackNbr);
-        handleTrackPlay(trackNbr);
-    }
-
     if (isMobile()) {
-        $('.icon.soundcloud').click(function () {
-            openSoundcloud();
-        });
         $('.icon.contact').click(function () {
             openContact();
         });
         $('.icon.photo').click(function () {
             openContact();
         });
-        $('.icon.track').click(function () {
-            openTrack($(this))
-        });
     } else {
-        $('.icon.soundcloud').dblclick(function () {
-            openSoundcloud()
-        });
         $('.icon.contact').dblclick(function () {
             openContact()
         });
         $('.icon.photo').dblclick(function () {
             openPhoto()
-        });
-        $('.icon.track').dblclick(function () {
-            openTrack($(this))
         });
     }
 
